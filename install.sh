8ea01eb8a564fdd50ffc4d7d21e4bbb3d828405c
Camille Tjhoa <ctjhoa@linagora.com>
2014-06-24 13:13:18 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Find relative path to the repository directory
+reposdir="$(dirname "$0")"
+
+# Obtain absolute path by entering the directory
+cd $reposdir
+confdir=`pwd`
+
 makepkg -fi --noconfirm
