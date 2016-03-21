4d8fe717de643a401296d4fbf71b464bd652e8de
Camille TJHOA <camille@contract-live.com>
2016-03-21 09:40:11 +0100

    Invert diff

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@
 curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/st --output PKGBUILD.def
 curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/config.h?h=packages/st --output config.h.def
 
-vimdiff PKGBUILD.def PKGBUILD
-vimdiff config.h.def config.h
+vimdiff PKGBUILD PKGBUILD.def
+vimdiff config.h config.h.def
 updpkgsums
